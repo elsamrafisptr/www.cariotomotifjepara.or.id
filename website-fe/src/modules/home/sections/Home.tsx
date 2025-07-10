1b8a66e0074296ef790d2ca74ca311d09b675994
Elsam Rafi Saputra <134671598+elsamrafisptr@users.noreply.github.com>
2025-07-10 16:01:00 +0700

    fix: brand marque

--- website-fe/src/modules/home/sections/Home.tsx
+++ website-fe/src/modules/home/sections/Home.tsx
@@ -1,5 +1,18 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
 import CarouselHeadline from './CarouselHeadline'
 import ServicesCard from './ServicesCard'
+
+import { Marquee } from '@/components/magicui/marquee'
+
+const BrandMarquee = [
+  { src: '/honda-white-logo.png', href: '/brands/motorcycle/honda' },
+  { src: '/kawasaki-logo.png', href: '/brands/motorcycle/kawasaki' },
+  { src: '/piaggio-logo.png', href: '/brands/motorcycle/piaggio' },
+  { src: '/suzuki-logo.png', href: '/brands/motorcycle/suzuki' },
+  { src: '/yamaha-logo.png', href: '/brands/motorcycle/yamaha' }
+]
 
 const Home = () => {
   return (
@@ -11,6 +24,24 @@ const Home = () => {
         showNavigation={true}
         showDots={true}
       />
+
+      <Marquee pauseOnHover className="bg-white py-6 [--duration:20s]">
+        {BrandMarquee.map((item, index) => (
+          <Link
+            href={item.href}
+            key={index}
+            className="aspect-video w-64 rounded-lg border hover:cursor-pointer hover:border-gray-500 md:w-80"
+          >
+            <Image
+              src={item.src}
+              alt={`Brand ${index}`}
+              width={1024}
+              height={1024}
+              className="w-full rounded-lg object-cover"
+            />
+          </Link>
+        ))}
+      </Marquee>
 
       <ServicesCard />
 
