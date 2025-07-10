fa954cdde702af5bce3fcec9e73bbcd00fbb670a
Elsam Rafi Saputra <134671598+elsamrafisptr@users.noreply.github.com>
2025-07-10 21:50:08 +0700

    fix: responsive

--- website-fe/src/app/(marketing)/brands/page.tsx
+++ website-fe/src/app/(marketing)/brands/page.tsx
@@ -5,16 +5,16 @@ import { BrandMarquee } from '@/modules/home/sections/Home'
 
 const BrandsPage = () => {
   return (
-    <section className="min-h-screen w-full bg-white px-30 py-12">
+    <section className="min-h-screen w-full bg-white px-5 py-12 sm:px-30">
       <div>
         <h1 className="text-3xl font-semibold">Brand/Merek Motor Tersedia</h1>
-        <div className="mt-6 grid grid-cols-4 items-center gap-8">
+        <div className="mt-4 grid grid-cols-2 items-center gap-4 sm:gap-6 md:mt-6 md:grid-cols-4 md:gap-8">
           {BrandMarquee.map((item, index) => {
             return (
               <Link
                 href={item.href}
                 key={index}
-                className="aspect-video w-full rounded-lg border hover:cursor-pointer hover:border-gray-500 md:w-80"
+                className="aspect-video w-full rounded border hover:cursor-pointer hover:border-gray-500 md:w-80 md:rounded-lg"
               >
                 <Image
                   src={item.src}
