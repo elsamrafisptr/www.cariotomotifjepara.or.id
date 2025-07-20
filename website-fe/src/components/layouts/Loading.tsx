83a56a14f88f0571c4993e89185e56510e20db48
Elsam Rafi Saputra <134671598+elsamrafisptr@users.noreply.github.com>
2025-07-20 16:11:13 +0700

    fix

--- website-fe/src/components/layouts/Loading.tsx
+++ website-fe/src/components/layouts/Loading.tsx
@@ -1,5 +1,12 @@
+import { LoaderCircle } from 'lucide-react'
+
 const Loading = () => {
-  return <div>Loading...</div>
+  return (
+    <section className="flex h-screen w-full flex-col items-center justify-center">
+      <LoaderCircle className="animate-spin" />
+      <p>Loading...</p>
+    </section>
+  )
 }
 
 export default Loading
--- website-fe/src/modules/product/sections/Product.tsx
+++ website-fe/src/modules/product/sections/Product.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+'use client'
 
 import honda_new_motorcycles from '@/common/contents/products/honda/new-motorcycle-contents'
 
@@ -7,8 +7,8 @@ import MotorProductCard from '@/components/elements/MotorProductCard'
 const Product = () => {
   return (
     <section className="min-h-screen bg-white px-5 md:px-44">
-      <div className="grid grid-cols-1 gap-x-6 gap-y-8 py-8 md:grid-cols-3 md:gap-x-16 md:py-16">
-        {honda_new_motorcycles.splice(0, 10).map((item, index) => {
+      <div className="grid grid-cols-1 gap-x-6 gap-y-8 md:grid-cols-3 md:gap-x-16 md:py-16">
+        {honda_new_motorcycles.map((item, index) => {
           return (
             <MotorProductCard
               key={index}
