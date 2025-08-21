e2d25e2fc9dd5b1eca167721557f9319192cb8d2
Elsam Rafi Saputra <134671598+elsamrafisptr@users.noreply.github.com>
2025-08-21 10:16:09 +0700

    feat: action integration for login

--- website-fe/src/modules/auth/login/login-form.tsx
+++ website-fe/src/modules/auth/login/login-form.tsx
@@ -1,3 +1,12 @@
+'use client'
+
+import Link from 'next/link'
+
+import { EyeClosedIcon, EyeIcon, RefreshCwIcon } from 'lucide-react'
+import { useState } from 'react'
+
+import useLoginForm from './action'
+
 import { cn } from '@/lib/utils'
 
 import { Button } from '@/components/ui/button'
@@ -8,13 +17,24 @@ import {
   CardHeader,
   CardTitle
 } from '@/components/ui/card'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
 
 export default function LoginForm({
   className,
   ...props
 }: React.ComponentProps<'div'>) {
+  const [seen, setSeen] = useState<boolean>(false)
+
+  const { form, isPending, onSubmit } = useLoginForm()
+
   return (
     <div className={cn('flex flex-col gap-6', className)} {...props}>
       <Card>
@@ -23,42 +43,96 @@ export default function LoginForm({
           <CardDescription>Login with your account</CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
-            <div className="grid gap-6">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
               <div className="grid gap-6">
-                <div className="grid gap-3">
-                  <Label htmlFor="email">Email</Label>
-                  <Input id="email" type="email" placeholder="m@example.com" required />
-                </div>
-                <div className="grid gap-3">
-                  <div className="flex items-center">
-                    <Label htmlFor="password">Password</Label>
-                    <a
-                      href="#"
-                      className="ml-auto text-xs underline-offset-4 hover:underline"
-                    >
-                      Forgot your password?
-                    </a>
-                  </div>
-                  <Input id="password" type="password" required />
-                </div>
-                <Button type="submit" className="w-full">
-                  Login
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem className="grid gap-3">
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="email"
+                          placeholder="m@example.com"
+                          disabled={isPending}
+                          required
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem className="grid gap-3">
+                      <div className="flex items-center">
+                        <FormLabel>Password</FormLabel>
+                        <Link
+                          href="#"
+                          className="ml-auto text-xs underline-offset-4 hover:underline"
+                        >
+                          Forgot your password?
+                        </Link>
+                      </div>
+                      <FormControl>
+                        <div className="relative">
+                          <Button
+                            type="button"
+                            variant="ghost"
+                            className="absolute top-1/2 right-1 -translate-y-1/2 hover:cursor-pointer hover:bg-transparent"
+                            onClick={() => setSeen(!seen)}
+                          >
+                            {seen ? (
+                              <EyeIcon className="size-4" />
+                            ) : (
+                              <EyeClosedIcon className="size-4" />
+                            )}
+                          </Button>
+                          <Input
+                            type={seen ? 'text' : 'password'}
+                            required
+                            disabled={isPending}
+                            {...field}
+                          />
+                        </div>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button
+                  type="submit"
+                  disabled={isPending}
+                  className="w-full cursor-pointer"
+                >
+                  {isPending ? (
+                    <>
+                      <RefreshCwIcon className="mr-2 h-4 w-4 animate-spin" />
+                      Loading...
+                    </>
+                  ) : (
+                    'Login'
+                  )}
                 </Button>
               </div>
-              <div className="text-center text-sm">
+              <div className="mt-6 text-center text-sm">
                 Don&apos;t have an account?{' '}
-                <a href="#" className="underline underline-offset-4">
+                <Link href="#" className="underline underline-offset-4">
                   Register
-                </a>
+                </Link>
               </div>
-            </div>
-          </form>
+            </form>
+          </Form>
         </CardContent>
       </Card>
       <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a> and{' '}
-        <a href="#">Privacy Policy</a>.
+        By clicking continue, you agree to our <Link href="#">Terms of Service</Link>{' '}
+        and <Link href="#">Privacy Policy</Link>.
       </div>
     </div>
   )
